6781dce7446739b66fee0b4bad54c95e89e43bdf
Damian Caetano <5100@holbertonschool.com>
2022-08-23 19:10:09 -0700

    Create the task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+void swap(listint_t *first, listint_t *second);
+/**
+ * insertion_sort_list - insertion sort algorithm
+ * @list: is a list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *front = *list;
+	listint_t *back = NULL;
+
+	while (front != NULL)
+	{
+		back = front->next;
+		while (back != NULL && back->prev != NULL && back->n < back->prev->n)
+		{
+			swap(back, back->prev);
+			print_list(*list);
+			back = back->prev;
+		}
+		front = front->next;
+	}
+}
+
+/**
+ * swap - swap two nodes
+ * @first: is a node
+ * @second: is a node
+ */
+
+void swap(listint_t *first, listint_t *second)
+{
+	int value;
+
+	value = first->n;
+	(*(int *)&(first->n)) = second->n;
+	(*(int *)&(second->n)) = value;
+}
