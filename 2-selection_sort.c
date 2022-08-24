a9c4d8b62f0730b7348b47be13eef383b8d6ebe2
Damian Caetano <5100@holbertonschool.com>
2022-08-23 19:39:42 -0700

    Create the task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sort algorithms in selection sort
+ * @array: is an array
+ * @size: is a size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
