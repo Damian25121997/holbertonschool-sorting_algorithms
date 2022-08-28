bca106d29875eb7d908e0555745031536e8b60a2
Damian Caetano <5100@holbertonschool.com>
2022-08-28 13:58:10 -0700

    fix errors of QuikSort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,7 +2,7 @@
 
 void swap(int *i, int *j);
 void quicksort_recursion(int *array, size_t size, int low, int high);
-int partition(int *array, int low, int high);
+int partition(int *array, size_t size, int low, int high);
 
 /**
  * quick_sort - sort algorithms in Quicksort
@@ -44,8 +44,7 @@ void quicksort_recursion(int *array, size_t size, int low, int high)
 
 	if (low < high)
 	{
-		pivot = partition(array, low, high);
-		print_array(array, size);
+		pivot = partition(array, size, low, high);
 		quicksort_recursion(array, size, low, pivot - 1);
 		quicksort_recursion(array, size, pivot + 1, high);
 	}
@@ -54,25 +53,30 @@ void quicksort_recursion(int *array, size_t size, int low, int high)
 /**
  * partition - is a function of pivot
  * @array: is an array
+ * @size: is the size of array
  * @low: is a first element of an array
  * @high: is a last element of an array
  * Return: sub-array
  */
 
-int partition(int *array, int low, int high)
+int partition(int *array, size_t size, int low, int high)
 {
 	int pivot_value = array[high];
-	int i = low;
+	int i = low - 1;
 	int j = low;
 
-	for (j = low; j < high; j++)
+	for (; j < high; j++)
 	{
 		if (array[j] <= pivot_value)
 		{
-			swap(&array[i], &array[j]);
 			i++;
+			swap(&array[i], &array[j]);
+			if (array[i] != array[j])
+				print_array(array, size);
 		}
 	}
-	swap(&array[i], &array[high]);
-	return (i);
+	swap(&array[i + 1], &array[high]);
+	if (array[i + 1] != array[high])
+		print_array(array, size);
+	return (i + 1);
 }
